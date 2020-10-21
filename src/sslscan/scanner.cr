91ac912fefcd52b036a30eb7307fe1b289d26842
Sijawusz Pur Rahnama <sija@sija.pl>
2020-10-21 15:56:40 +0200

    Add some additional options

--- src/sslscan/scanner.cr
+++ src/sslscan/scanner.cr
@@ -2,12 +2,19 @@ module SSLScan
   module Scanner
     include Parser
 
-    def scan(host : String, port : Int32? = nil, client_ciphers = false, times = false) : Report
+    def scan(host : String, port : Int32? = nil, sni_name : String? = nil, ip_version : Symbol? = nil, client_ciphers : Bool = false, times : Bool = false, sleep : Time::Span? = nil, timeout : Time::Span? = nil) : Report
       host += ":#{port}" if port
 
       args = %w[--no-colour]
+      args << "--sni-name=#{sni_name}" if sni_name
+      case ip_version
+      when :ipv4 then args << "--ipv4"
+      when :ipv6 then args << "--ipv6"
+      end
       args << "--show-ciphers" if client_ciphers
       args << "--show-times" if times
+      args << "--sleep=#{sleep.total_milliseconds.to_i}" if sleep
+      args << "--timeout=#{timeout.total_seconds.to_i}" if timeout
       args << host
 
       document = run_xml(args)
