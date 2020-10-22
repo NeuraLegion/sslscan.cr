80f70ef7859a276e874955f47d144c368bf0c340
Sijawusz Pur Rahnama <sija@sija.pl>
2020-10-22 21:21:30 +0200

    Add some spec basic helpers, fixtures and coverage for SSLScan.version

--- spec/sslscan_spec.cr
+++ spec/sslscan_spec.cr
@@ -1,9 +1,15 @@
 require "./spec_helper"
 
 Spectator.describe SSLScan do
-  # TODO: Write tests
+  describe ".version" do
+    mock SSLScan do
+      stub run_xml(args : Array(String)) do
+        xml_fixture("version.xml")
+      end
+    end
 
-  it "works" do
-    true.should be_true
+    it "returns version string from document[version] attribute" do
+      expect(SSLScan.version).to eq "2.0.4-static"
+    end
   end
 end
